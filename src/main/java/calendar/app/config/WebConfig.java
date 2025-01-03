f077a2f6d2a8f62392776cfdef5cd801960f99e8
Amit Aryan <aryanamit122@gmail.com>
2025-01-03 16:02:07 +0530

    Update WebConfig.java

--- src/main/java/calendar/app/config/WebConfig.java
+++ src/main/java/calendar/app/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React app origin
+                .allowedOrigins("https://amitaryansingh.github.io") // React app origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
